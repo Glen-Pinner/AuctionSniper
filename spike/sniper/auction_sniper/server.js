1272f83a3485e69ae3ab4dfb867fbe9b141c2111
Glen Pinner <glen.pinner@gmail.com>
2014-03-26 16:30:15 -0500

    Connected Express server to RabbitMQ

--- spike/sniper/auction_sniper/server.js
+++ spike/sniper/auction_sniper/server.js
@@ -26,6 +26,75 @@
 
     server.listen(app.get('port'), function() {
         console.log("Express app started on port " + app.get('port'));
+
+        connectToFakeAuctionServer();
     });
+
+    function connectToFakeAuctionServer() {
+        'use strict';
+
+        var amqp = require('amqplib'),
+            when = require('when');
+
+        var ADMIN_EXCHANGE   = 'admin',
+            AUCTION_EXCHANGE = 'auction',
+            ADMIN_QUEUE      = 'admin',
+            COMMAND_QUEUE    = 'command';
+
+        amqp.connect('amqp://localhost').then(function(connection) {
+            return when(connection.createChannel().then(function(channel) {
+                var ok = channel.assertExchange(ADMIN_EXCHANGE, 'direct', { autoDelete: false });
+
+                ok = ok.then(function() {
+                    return channel.assertExchange(AUCTION_EXCHANGE, 'direct', { autoDelete: false });
+                });
+
+                // Create admin queue and bind it to exchange
+                ok = ok.then(function() {
+                    return channel.assertQueue(ADMIN_QUEUE, {
+                        autoDelete: false
+                    });
+                });
+
+                ok = ok.then(function(queueOk) {
+                    return channel.bindQueue(queueOk.queue, ADMIN_EXCHANGE, '').then(function() {
+                        return queueOk.queue;
+                    })
+                });
+
+                // Create command queue and bind it
+                ok = ok.then(function() {
+                    return channel.assertQueue(COMMAND_QUEUE, {
+                        autoDelete: false,
+                        noAck: true
+                    });
+                });
+
+                ok = ok.then(function(queueOk) {
+                    return channel.bindQueue(queueOk.queue, AUCTION_EXCHANGE, '').then(function() {
+                        return queueOk.queue;
+                    })
+                });
+
+                return ok.then(function() {
+                    var message = 'SOL Version: 1.1; Command: START_SELLING';
+                    channel.sendToQueue(ADMIN_QUEUE, new Buffer(message));
+                    console.log(' [x] Sent %s', message);
+
+                    message = 'SOL Version: 1.1; Command: JOIN';
+                    channel.sendToQueue(COMMAND_QUEUE, new Buffer(message));
+                    console.log(' [x] Sent %s', message);
+
+                    message = 'SOL Version: 1.1; Command: BID; Price: 199';
+                    channel.sendToQueue(COMMAND_QUEUE, new Buffer(message));
+                    console.log(' [x] Sent %s', message);
+
+                    return channel.close();
+                });
+            })).ensure(function() { connection.close(); });
+        }).then(null, console.warn);
+
+        console.log('Connected to Fake Auction Server');
+    }
 
 })();
